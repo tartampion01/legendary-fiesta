ed3a15561e5387e37fb521d85ca4f3e062c14011
Philippe Tourigny <philtourigny@gmail.com>
2019-05-21 20:53:29 -0400

    50 caractères pour facture

--- public_html/feuillederoute.php
+++ public_html/feuillederoute.php
@@ -214,7 +214,7 @@
             <div class="col-xs-3">
                 <div class="row">
                     <div class="col-xs-12">
-                        <input type="text" name="tbFacture[]" id="tbFacture${counter}" value="" maxlength="20"  class="input"></input><br>
+                        <input type="text" name="tbFacture[]" id="tbFacture${counter}" value="" maxlength="50"  class="input"></input><br>
                     </div>
                 </div>
             </div>

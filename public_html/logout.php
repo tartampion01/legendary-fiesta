a9f810b2bcd2cb44232271696c2bfc60590b9e89
Philippe Tourigny <philtourigny@gmail.com>
2019-06-02 15:12:43 -0400

    Clear cookie data when logout is clicked

--- public_html/logout.php
+++ public_html/logout.php
@@ -1,15 +1,22 @@
 <?php
 require_once(dirname(__DIR__).'/_includes/commonIncludes.php');
 
-setcookie("username", "", time()-3600);
-setcookie("succursale", "", time()-3600);
+setcookie('USERNAME', "", time()-3600);
+setcookie('SUCCURSALE', "", time()-3600);
+setcookie('ID_USER', "", time()-3600);
+setcookie('LEVEL', "", time()-3600);
 
 IL_Session::w(IL_SessionVariables::USERNAME,false);
+IL_Session::w(IL_SessionVariables::SUCCURSALE,false);
 IL_Session::w(IL_SessionVariables::ID_USER,false);
 IL_Session::w(IL_SessionVariables::LEVEL,false);
-IL_Session::w(IL_SessionVariables::SUCCURSALE,false);
 
 IL_Session::destroy();
 
 header('Location: ' . "login.php");
+
+//var_dump($_SESSION);
+//echo '</br>';
+//var_dump($_COOKIE);
+
 ?>

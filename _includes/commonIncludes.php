cf8e44c5b3c1819d2dad8c0002726d9055e59a22
Philippe Tourigny <philtourigny@gmail.com>
2019-06-02 15:11:37 -0400

    Added include for error class

--- _includes/commonIncludes.php
+++ _includes/commonIncludes.php
@@ -4,7 +4,7 @@
 error_reporting(0);
 // Show errors on webpage
 //error_reporting(E_ALL);
-    
+
 $applicationConfig = parse_ini_file(dirname(__DIR__).'/_configs/application.ini');
 
 // CONNECTION DB
@@ -12,6 +12,7 @@ require_once(dirname(__DIR__). '/_includes/objects/database.php');
 // FUNCTIONS
 require_once(dirname(__DIR__). '/_includes/functions/dbFunctions.php');
 // OBJECTS
+require_once(dirname(__DIR__). '/_includes/objects/IL_Errors.php');
 require_once(dirname(__DIR__). '/_includes/objects/IL_Session.php');
 require_once(dirname(__DIR__). '/_includes/objects/IL_Users.php');
 require_once(dirname(__DIR__). '/_includes/objects/IL_PageLink.php');

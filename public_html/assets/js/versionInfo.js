034d9ee724a5a51f1109b72cf4b8069a69c93cd2
Philippe Tourigny <philtourigny@gmail.com>
2019-07-24 20:49:12 -0400

    new

---
+++ public_html/assets/js/versionInfo.js
@@ -0,0 +1,23 @@
+/****************************************************/
+/* JS FILE USED TO TEACK VERSION CHANGES ON TABLETS */
+/****************************************************/
+/*
+/* The CURRENT_VERSION number from THIS file needs to be +1 from the value defined in AndroidManifest.xml
+ * in : <meta-data android:value="10" android:name="APK_version"></meta-data>
+ * 
+ * 11 = Initial Release 17-07-2019
+ *      Includes changes to keep username/password on login page
+ *      Oncludes this JS to keeptrack of versions
+ * 12 = 
+ * 13 = 
+ * 14 = 
+ * 15 = 
+ * 16 = 
+ * 17 = 
+ * 18 = 
+ * 19 = 
+ * ...
+ */
+var CURRENT_VERSION = 11;
+
+
